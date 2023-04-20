3ebd2b0d5fed4443c695bf858849935352aae518
Marco <42436088+MasterMs@users.noreply.github.com>
2023-04-20 17:58:26 -0400

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:msiciliano_weather_app/home_screen.dart';
+import 'package:msiciliano_weather_app/location_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,8 +16,48 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(),
+      home: MainScreen(),
     );
   }
 }
 
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _screens = [
+    HomeScreen(),
+    LocationsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: 'Locations',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/weather_api.dart
+++ lib/weather_api.dart
@@ -1,41 +1,36 @@
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:msiciliano_weather_app/weather.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:msiciliano_weather_app/weather.dart';
 
 class WeatherApi {
-  static const apiKey = '24a6dca649f96e67e9208954d79b1f85';
-  static const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  static const String _apiKey = '24a6dca649f96e67e9208954d79b1f85';
+  static const String _baseUrl =
+      'https://api.openweathermap.org/data/3.0/onecall';
 
-  static Future<Weather> getWeatherByLocation(double lat, double lon) async {
-    final response = await http.get(Uri.parse('$baseUrl?lat=$lat&lon=$lon&appid=$apiKey&units=metric'));
+  static Future<Weather?> getWeatherByCityName(String cityName) async {
+    final response = await http.get(
+        '$_baseUrl?q=$cityName&appid=$_apiKey&units=metric' as Uri);
+
     if (response.statusCode == 200) {
-      return parseWeatherData(jsonDecode(response.body));
+      return Weather.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load weather data');
+      return null;
     }
   }
 
-  static Weather parseWeatherData(Map<String, dynamic> jsonData) {
-    final cityName = jsonData['name'];
-    final temperature = jsonData['main']['temp'];
-    final description = jsonData['weather'][0]['description'];
-    final weatherIconCode = jsonData['weather'][0]['icon'];
-    final weatherIcon = getWeatherIcon(weatherIconCode);
-    return Weather(cityName: cityName, temperature: temperature, description: description, weatherIcon: weatherIcon);
-  }
+  static Future<Weather?> getWeatherByLocation(double lon, double lat) async {
+    final response = await http.get(
+        '$_baseUrl?$lat=50&lon=75&appid=$_apiKey&units=metric' as Uri);
 
-  static IconData getWeatherIcon(String iconCode) {
-    switch (iconCode) {
-      case '01d':
-        return Icons.wb_sunny;
-      case '01n':
-        return Icons.nightlight_round;
-      default:
-        return Icons.cloud;
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      return null;
     }
   }
+
+
 }
